03cdefb42e1bbababfddef5ed3f7f6dc9d0c9bb1
Your Name <you@example.com>
2024-03-28 16:41:32 -0700

    Add solution to exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+  let reverse  = ``;
+  for(let i = input.length - 1; i >= 0; --i) {
+    reverse += input[i];
+  }
+  return reverse;
 };
 
 // Do not edit below this line
