14141b37176c84ac8038b02415d039c0aa7d13b5
Your Name <you@example.com>
2024-03-28 17:17:22 -0700

    Add completed solution to exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...itemsToRemove) {
+  let result = array.filter(
+    (itemToRemove) => !itemsToRemove.includes(itemToRemove)
+    );
+  return result;
 };
 
 // Do not edit below this line
