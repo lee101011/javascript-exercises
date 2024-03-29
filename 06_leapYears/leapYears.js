58a6bcfc947840e1703eb6480a905e91f18e21dd
Your Name <you@example.com>
2024-03-28 17:51:56 -0700

    Add completed exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  if (!Number.isInteger(year) || year < 0) return `ERROR`;
+  let a = year % 100 === 0;
+  let b = year % 400 === 0 ;
 
+  return (year % 100 === 0 && year % 400 === 0 || year % 4 === 0 && year % 100 !== 0);
 };
 
 // Do not edit below this line
